35ff4e3eb462952651e2042f408083e48b0cde8a
ritamak <ritapmak@gmail.com>
2021-12-02 22:02:10 +0000

    styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,8 @@
-import { useContext } from "react";
 import Header from "./Header";
 import styled from "styled-components";
-import { ThemeContext } from "../store/ThemeContext";
-import { createGlobalStyle } from "styled-components";
 
 const ContentContainer = styled.section`
-  background: ${(props) => (props.darkMode ? "#615849" : "#f3e5c8")};
+  background: #f3e5c8;
   text-align: center;
   font-family: "Nunito", sans-serif;
   display: flex;
@@ -17,26 +14,11 @@ const ContentContainer = styled.section`
   transition: all 0.5s linear;
 `;
 
-const GlobalStyles = createGlobalStyle`
-  body {
-    background: ${(props) => (props.darkMode ? "#615849" : "#f3e5c8")};
-    transition: all 0.50s linear;
-  }
-  `;
-
 const Layout = ({ headerLink, headerText, children }) => {
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
   return (
     <>
-      <GlobalStyles darkMode={darkMode} />
-      <ContentContainer darkMode={darkMode}>
-        <Header
-          theme={theme}
-          link={headerLink}
-          text={headerText}
-          darkMode={darkMode}
-        />
+      <ContentContainer>
+        <Header link={headerLink} text={headerText} />
         {children}
       </ContentContainer>
     </>
--- src/utils/GlobalStyles.js
+++
@@ -1,7 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-export const GlobalStyles = createGlobalStyle`
-  body {
-    background: ${(props) => (props.darkMode ? "black" : "white")};
-    transition: all 0.50s linear;
-  }
-  `;
